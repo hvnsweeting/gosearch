04d96a7fe59c8959bdcbc03dc31df4e8825d902d
Viet Hung Nguyen <hvn@familug.org>
2017-08-31 14:36:22 +0700

    Refactor: use main only for processing flags then call funcs

--- main.go
+++ main.go
@@ -15,6 +15,21 @@ import (
 	"strings"
 )
 
+// TODO handle non package links
+/*
+gocql.github.io
+godoc.org/labix.org/v2/mgo
+mattn.github.io/go-gtk
+eclipse.org/paho/clients/golang
+www.consul.io
+nsq.io
+onsi.github.io/ginkgo
+labix.org/gocheck
+onsi.github.io/gomega
+aahframework.org
+gobuffalo.io
+rest-layer.io
+*/
 var found bool
 var wanted string
 var categoryFlag = flag.String("c", "", "Show packages in `category`. Use `all` for list of all categories.")
@@ -45,21 +60,6 @@ var (
 	reLinkWithDescription = regexp.MustCompile(`\* (\[.*\]\(.*\)) - (\S.*)`)
 )
 
-// TODO handles
-/*
-gocql.github.io
-godoc.org/labix.org/v2/mgo
-mattn.github.io/go-gtk
-eclipse.org/paho/clients/golang
-www.consul.io
-nsq.io
-onsi.github.io/ginkgo
-labix.org/gocheck
-onsi.github.io/gomega
-aahframework.org
-gobuffalo.io
-rest-layer.io
-*/
 func rawData() (rawdata []byte, err error) {
 	rawdata, err = Asset("data/README.md")
 	return rawdata, err
@@ -71,32 +71,31 @@ func myUsage() {
 	flag.PrintDefaults()
 }
 
-func main() {
-	flag.Usage = myUsage
-	rawdata, err := rawData()
-	if err != nil {
-		log.Fatal("Cannot read data")
-	}
+type Filter func(name string, pkg Package) bool
 
-	flag.Parse()
+func categoryFilter(name string, pkg Package) bool {
+	return name == pkg.category
+}
+func nameFilter(name string, pkg Package) bool {
+	return name == pkg.name
+}
+func passThrought(name string, pkg Package) bool {
+	return true
+}
 
+func notFound() {
+	fmt.Printf("Not found `%s`\n", wanted)
+	os.Exit(1)
+}
+
+func searchPackage(wanted string, lines []string, filter Filter) []Package {
 	var matched, containsLink, noDescription bool
 	var category string
-	var categories = map[string]int{}
-
-	lines := strings.Split(string(rawdata), "\n")
-	if *rawFlag {
-		for _, line := range lines {
-			fmt.Println(line)
-		}
-		return
-	}
-
+	var pkgs []Package
 	for _, line := range lines {
 		line = strings.Trim(line, " ")
 		if strings.HasPrefix(line, "## ") {
 			category = strings.ToLower(line[3:])
-			categories[category] = 0
 		}
 		containsLink = reContainsLink.MatchString(line)
 		if containsLink {
@@ -116,55 +115,81 @@ func main() {
 				pkg := getNameAndDesc(left, right)
 				pkg.category = category
 
-				categories[pkg.category] += 1
-
-				if *categoryFlag != "" {
-					if *categoryFlag == "all" {
-						found = true
-						// defer print result before exitting
-					}
-					if *categoryFlag == pkg.category {
-						fmt.Printf("%s - %s\n", pkg.pkg, pkg.desc)
-						found = true
-					}
-				} else {
-
-					if flag.NArg() == 0 {
-						flag.Usage()
-						os.Exit(1)
-					}
-					wanted = flag.Args()[0]
-
-					if wanted == pkg.name {
-						fmt.Printf("Package: %s\n", pkg.pkg)
-						fmt.Printf("Category: %s\n", pkg.category)
-						fmt.Printf("Description-en: %s\n", pkg.desc)
-
-						found = true
-					}
+				if filter(wanted, pkg) {
+					pkgs = append(pkgs, pkg)
 				}
 			}
 
 		}
 	}
+	return pkgs
 
-	if *categoryFlag == "all" {
-		var cats []string
+}
 
-		for k, _ := range categories {
-			cats = append(cats, k)
-		}
-		sort.Strings(cats)
-
-		for _, k := range cats {
-			fmt.Printf("%s: %d packages\n", k, categories[k])
-
-		}
-
+func main() {
+	flag.Usage = myUsage
+	rawdata, err := rawData()
+	if err != nil {
+		log.Fatal("Cannot read data")
 	}
 
-	if !found {
-		fmt.Printf("Not found `%s`\n", wanted)
-		os.Exit(1)
+	flag.Parse()
+
+	lines := strings.Split(string(rawdata), "\n")
+	if *rawFlag {
+		for _, line := range lines {
+			fmt.Println(line)
+		}
+		return
+	}
+
+	var pkgs []Package
+	if *categoryFlag != "" {
+		wanted = *categoryFlag
+		if wanted == "all" {
+			categories := map[string]int{}
+			var cats []string
+			pkgs = searchPackage(wanted, lines, passThrought)
+			for _, pkg := range pkgs {
+				categories[pkg.category] += 1
+			}
+
+			for k, _ := range categories {
+				cats = append(cats, k)
+			}
+			sort.Strings(cats)
+
+			for _, k := range cats {
+				fmt.Printf("%s: %d packages\n", k, categories[k])
+			}
+
+		} else {
+			pkgs = searchPackage(wanted, lines, categoryFilter)
+
+			if len(pkgs) == 0 {
+				notFound()
+			}
+			for _, pkg := range pkgs {
+				fmt.Printf("%s - %s\n", pkg.pkg, pkg.desc)
+			}
+		}
+	} else {
+
+		if flag.NArg() == 0 {
+			flag.Usage()
+			os.Exit(1)
+		}
+		wanted = flag.Args()[0]
+
+		pkgs = searchPackage(wanted, lines, nameFilter)
+		if len(pkgs) == 0 {
+			notFound()
+		}
+		for _, pkg := range pkgs {
+			fmt.Printf("Package: %s\n", pkg.pkg)
+			fmt.Printf("Category: %s\n", pkg.category)
+			fmt.Printf("Description-en: %s\n", pkg.desc)
+		}
+
 	}
 }
