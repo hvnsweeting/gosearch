7598c409732afa6d8b1c6e20527cb475c216a20d
Viet Hung Nguyen <hvn@familug.org>
2017-09-01 13:54:49 +0700

    Add test for corner case with (deprecated)

--- gosearch_test.go
+++ gosearch_test.go
@@ -1,7 +1,15 @@
 package main
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
-func TestFoo(t *testing.T) {
-
+func TestMarkDownLinkWithDeprecatedNote(t *testing.T) {
+	s := "* [go-lang-idea-plugin](https://github.com/go-lang-plugin-org/go-lang-idea-plugin) (deprecated) - The previous Go plugin for IntelliJ (JetBrains) IDEA, now replaced by the official plugin (above)."
+	parts := strings.Split(s, " - ")
+	pkg := getNameAndDesc(parts[0], parts[1])
+	if pkg.pkg != "github.com/go-lang-plugin-org/go-lang-idea-plugin" {
+		t.Errorf("parser failed to parse %s. Got: %s", s, pkg.pkg)
+	}
 }
